9688acb20e51460492295cb87cfd7fe5e82dcea5
VladyslavBondyk <vladyslav.bondyk@gmail.com>
2023-08-01 00:16:18 +0200

    2nd task is DONE

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Bird bird = new Bird();
+        Dog dog = new Dog();
+
+        System.out.println("Bird Description: " + bird.getDescription());
+        System.out.println("Dog Description: " + dog.getDescription());
+    }
+}
